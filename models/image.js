8a34cf91a5fe613cce7cae373c0282a389c63b53
Jae <giffordjacob0@gmail.com>
2022-12-28 15:33:48 +0800

    image schema updated to match available edits to photos

--- models/image.js
+++ models/image.js
@@ -5,6 +5,15 @@ const ImageSchema = new Schema({
   name: { type: String },
   img: {
     contentType: String,
+    filter: { type: String },
+    adjustments: {
+      brightness: { type: Number },
+      contrast: { type: Number },
+      saturation: { type: Number },
+      temperature: { type: Number },
+      fade: { type: Number },
+      vignette: { type: Number },
+    },
   },
   url: { type: String, required: true },
 });
