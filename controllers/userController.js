ccaede229f7aef11175270e918dc9a80498df14c
Jae <giffordjacob0@gmail.com>
2023-01-14 14:54:31 +0800

    user search enabled

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 exports.users_get = (req, res, next) => {
   // find all users
-  const { firstname, lastname, username, isadmin, reqLimit, skipToPage } =
+  let { firstname, lastname, username, isadmin, reqLimit, skipToPage, q } =
     req.query;
   let resultsPerPage = 5;
   if (reqLimit) {
@@ -13,6 +13,22 @@ exports.users_get = (req, res, next) => {
     startIndex = Number(skipToPage) * resultsPerPage;
   }
 
+  // search query allows checking for real names, OR for usernames
+  // make sure in names allowable, whitespace is NOT allowed in UserName OR in any name field
+  // else this will throw some wild results.
+
+  if (q) {
+    // wwhitespace test regex, if whitespace is included, query is considered a firstname/lastname search.
+    if (/\s/.test(q)) {
+      nameArray = q.split(' ');
+      for (let i = 0; i < nameArray.length; i++) {
+        firstname = nameArray[0];
+        lastname = nameArray[1];
+      }
+    } else {
+      username = q;
+    }
+  }
   User.find({}).exec(function (err, users) {
     if (err) return next(err);
     else {
@@ -20,22 +36,52 @@ exports.users_get = (req, res, next) => {
       // filter by queries
       let results = [...users];
 
-      if (firstname) {
+      if (firstname && !lastname) {
         results = results.filter(
           (user) => user.firstName.toLowerCase() === firstname.toLowerCase()
         );
       }
-      if (lastname) {
+      if (lastname && !firstname) {
         results = results.filter(
           (user) => user.lastName.toLowerCase() === lastname.toLowerCase()
         );
       }
+      if (firstname && lastname) {
+        console.log('first last search');
+        results = results.filter(
+          (user) =>
+            // check first and last names for queried fields
+            user.firstName.toLowerCase().includes(firstname.toLowerCase()) &&
+            user.lastName.toLowerCase().includes(lastname.toLowerCase())
+        );
+      }
       if (username) {
+        console.log('pass user');
         console.log(username);
+        console.log(q);
+
         results = results.filter((user) =>
           user.userName.toLowerCase().includes(username.toLowerCase())
         );
+        console.log(results.length);
+        // search for user real name is username fails
+        if (q && !results.length) {
+          // check for firstname first
+          console.log('pass first');
+          // needs to check the whole array again because results is empty
+          results = [...users].filter((user) =>
+            user.firstName.toLowerCase().includes(q.toLowerCase())
+          );
+          if (!results.length) {
+            console.log('pass last');
+            // check for last name after
+            results = [...users].filter((user) =>
+              user.lastName.toLowerCase().includes(q.toLowerCase())
+            );
+          }
+        }
       }
+
       if (isadmin) {
         results = results.filter((user) => user.isAdmin);
       }
